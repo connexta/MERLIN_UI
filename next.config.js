a9b4d5128ecd01172ba4952f6275904f5c939bf1
Don Cruver <donald.cruver@connexta.com>
2022-08-18 15:20:44 -0700

    added kubernetes artifacts

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  basePath: '/merlin',
 }
 
 const withTM = require('next-transpile-modules')(['flexlayout-react']); // pass the modules you would like to see transpiled
